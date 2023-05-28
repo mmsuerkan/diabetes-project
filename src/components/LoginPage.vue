d5941eafed4e358aa48a28804c7fb41c6ef58f31
mmsuerkan <mmsuerkan@gmail.com>
2023-05-28 14:14:39 +0300

    twitter facebook github login added

--- src/components/LoginPage.vue
+++ src/components/LoginPage.vue
@@ -22,12 +22,23 @@
           ></v-text-field>
           <v-row>
             <v-col cols="12">
-              <v-btn :disabled="!valid" color="primary" class="mr-4" @click="enter">Sign in</v-btn>
+              <v-btn :disabled="!valid" color="primary" class="mr-4" @click="enter">Login</v-btn>
               <v-btn color="primary" @click="register" class="mr-4">Register</v-btn>
-              <v-btn color="primary" @click="googleSignIn">Sign in with Google</v-btn>
+              <v-btn color="primary"  dark @click="googleSignIn"  class="mr-4">
+                <svg-icon type="mdi" :path="googleIconPath"></svg-icon>
+              </v-btn>
+              <v-btn color="primary" dark @click="facebookSignIn"  class="mr-4">
+                <svg-icon type="mdi" :path="facebookIconPath"></svg-icon>
+              </v-btn>
+              <v-btn color="primary" dark @click="githubSignIn"  class="mr-4">
+                <svg-icon type="mdi" :path="githubIconPath"></svg-icon>
+              </v-btn>
+              <v-btn color="primary" dark @click="twitterSignIn"  class="mr-4">
+                <svg-icon type="mdi" :path="twitterIconPath"></svg-icon>
+              </v-btn>
             </v-col>
             <v-col cols="12">
-              <v-btn text @click="goToForgotPasswordPage">Forgot Password?</v-btn>
+              <v-btn text @click="goToForgotPasswordPage">Forgot Password</v-btn>
             </v-col>
           </v-row>
         </v-form>
@@ -37,23 +48,33 @@
 </template>
 
 <script>
-import { getAuth, signInWithEmailAndPassword, GoogleAuthProvider, signInWithPopup, sendEmailVerification } from "firebase/auth";
+import { getAuth, signInWithEmailAndPassword, GoogleAuthProvider, signInWithPopup, sendEmailVerification,FacebookAuthProvider,TwitterAuthProvider,
+GithubAuthProvider} from "firebase/auth";
 import { ref, set, getDatabase } from "firebase/database";
 import sweetAlert from "sweetalert";
 
+import SvgIcon from '@jamescoyle/vue-icon';
+import { mdiGoogle, mdiFacebook, mdiGithub, mdiTwitter } from '@mdi/js';
 export default {
+  components: {
+    SvgIcon
+  },
   data: () => ({
     valid: true,
     showPassword: false,
     email: "",
     password: "",
     emailRules: [
-      (v) => !!v || "E-mail is required",
-      (v) => /.+@.+/.test(v) || "E-mail must be valid",
+      (v) => !!v || "Email Required",
+      (v) => /.+@.+/.test(v) || "Please enter a valid email",
     ],
     passwordRules: [
-      (v) => !!v || "Password is required",
+      (v) => !!v || "Password Required",
     ],
+    googleIconPath: mdiGoogle,
+    facebookIconPath: mdiFacebook,
+    githubIconPath: mdiGithub,
+    twitterIconPath: mdiTwitter
   }),
   created() {
     const auth = getAuth();
@@ -76,13 +97,13 @@ export default {
         const userCredential = await signInWithEmailAndPassword(auth, this.email, this.password);
         const user = userCredential.user;
         if (!user.emailVerified) {
-          sweetAlert("Error", "Please verify your email before logging in.", "error");
+          sweetAlert("Hata", "Lütfen giriş yapmadan önce e-postanızı doğrulayın.", "error");
           return;
         }
-        sweetAlert("Success", `${user.email} You are logged in`, "success");
+        sweetAlert("Başarılı", `${user.email} Giriş yapıldı`, "success");
         this.$router.push({ name: "MainPage" });
       } catch (error) {
-        sweetAlert("Error", error.message, "error");
+        sweetAlert("Hata", error.message, "error");
       }
     },
     register() {
@@ -99,14 +120,77 @@ export default {
           // Kullanıcı henüz doğrulama yapmadığı için hemen hesap oluşturma işlemi gerçekleştirilmez.
           // Doğrulama yapılması için kullanıcıya e-posta gönderilir.
           await sendEmailVerification(user);
-          sweetAlert("Success", "Please verify your email address.", "success");
+          sweetAlert("Başarılı", "Lütfen e-posta adresinizi doğrulayın.", "success");
         } else {
           await this.createUserInDatabase(user);
-          sweetAlert("Success", `${user.email} You are logged in`, "success");
+          sweetAlert("Başarılı", `${user.email} Giriş yapıldı`, "success");
         }
         this.$router.push({ name: "MainPage" });
       } catch (error) {
-        sweetAlert("Error", error.message, "error");
+        sweetAlert("Hata", error.message, "error");
+      }
+    },
+    async facebookSignIn() {
+      const auth = getAuth();
+      const provider = new FacebookAuthProvider();
+
+      try {
+        const userCredential = await signInWithPopup(auth, provider);
+        const user = userCredential.user;
+        if (!user.emailVerified) {
+          // Kullanıcı henüz doğrulama yapmadığı için hemen hesap oluşturma işlemi gerçekleştirilmez.
+          // Doğrulama yapılması için kullanıcıya e-posta gönderilir.
+          await sendEmailVerification(user);
+          sweetAlert("Başarılı", "Lütfen e-posta adresinizi doğrulayın.", "success");
+        } else {
+          await this.createUserInDatabase(user);
+          sweetAlert("Başarılı", `${user.email} Giriş yapıldı`, "success");
+        }
+        this.$router.push({ name: "MainPage" });
+      } catch (error) {
+        sweetAlert("Hata", error.message, "error");
+      }
+    },
+    async githubSignIn() {
+      const auth = getAuth();
+      const provider = new GithubAuthProvider();
+
+      try {
+        const userCredential = await signInWithPopup(auth, provider);
+        const user = userCredential.user;
+        if (!user.emailVerified) {
+          // Kullanıcı henüz doğrulama yapmadığı için hemen hesap oluşturma işlemi gerçekleştirilmez.
+          // Doğrulama yapılması için kullanıcıya e-posta gönderilir.
+          await sendEmailVerification(user);
+          sweetAlert("Başarılı", "Lütfen e-posta adresinizi doğrulayın.", "success");
+        } else {
+          await this.createUserInDatabase(user);
+          sweetAlert("Başarılı", `${user.email} Giriş yapıldı`, "success");
+        }
+        this.$router.push({ name: "MainPage" });
+      } catch (error) {
+        sweetAlert("Hata", error.message, "error");
+      }
+    },
+    async twitterSignIn() {
+      const auth = getAuth();
+      const provider = new TwitterAuthProvider();
+
+      try {
+        const userCredential = await signInWithPopup(auth, provider);
+        const user = userCredential.user;
+        if (!user.emailVerified) {
+          // Kullanıcı henüz doğrulama yapmadığı için hemen hesap oluşturma işlemi gerçekleştirilmez.
+          // Doğrulama yapılması için kullanıcıya e-posta gönderilir.
+          await sendEmailVerification(user);
+          sweetAlert("Başarılı", "Lütfen e-posta adresinizi doğrulayın.", "success");
+        } else {
+          await this.createUserInDatabase(user);
+          sweetAlert("Başarılı", `${user.email} Giriş yapıldı`, "success");
+        }
+        this.$router.push({ name: "MainPage" });
+      } catch (error) {
+        sweetAlert("Hata", error.message, "error");
       }
     },
     async createUserInDatabase(user) {
@@ -118,9 +202,9 @@ export default {
           email: user.email,
           // Diğer kullanıcı bilgilerini burada ekleyebilirsiniz
         });
-        console.log("User created in database:", snapshot);
+        console.log("Kullanıcı veritabanında oluşturuldu:", snapshot);
       } catch (error) {
-        console.error("Error creating user in database:", error);
+        console.error("Kullanıcı veritabanında oluşturulurken hata oluştu:", error);
       }
     },
   },
